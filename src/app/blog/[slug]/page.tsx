fc52b66ba56447ccdb27133c298a94581197ddaa
lheumel <lheumel@mindbaz.com>
2024-12-01 19:39:03 +0100

    fix(blog): update interface to promise

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,15 +1,13 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import React from "react";
+import { notFound } from "next/navigation";
 
 interface BlogPostProps {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 }
 
-// Génère les chemins pour chaque article
+// Génère les chemins statiques pour chaque article
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join(process.cwd(), "posts"));
   return files.map((filename) => ({
@@ -18,8 +16,23 @@ export async function generateStaticParams() {
 }
 
 // Composant pour afficher un article
-export default function BlogPost({ params: { slug } }: BlogPostProps) {
+export default async function BlogPost({ params }: BlogPostProps) {
+  const resolvedParams = await params; // Résout la promesse
+  const { slug } = resolvedParams;
+
+  if (!slug) {
+    notFound(); // Renvoie une page 404 si `slug` est introuvable
+  }
+
+  // Chemin vers le fichier Markdown
   const markdownPath = path.join(process.cwd(), "posts", `${slug}.md`);
+
+  // Vérifie si le fichier existe
+  if (!fs.existsSync(markdownPath)) {
+    notFound(); // Génère une page 404 si l'article n'existe pas
+  }
+
+  // Lit et analyse le contenu du fichier Markdown
   const markdownWithMeta = fs.readFileSync(markdownPath, "utf-8");
   const { data: frontMatter, content } = matter(markdownWithMeta);
 
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -47,13 +47,13 @@ const Header = () => {
                   </a>
                 </li>
                 <li>
-                  <a
+                  <Link
                     className="text-gray-500 transition hover:text-gray-500/75"
                     href="/blog"
                   >
                     {" "}
                     Blog{" "}
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a
